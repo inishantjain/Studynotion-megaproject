7d1c37dc2e1de513b654e29232cded4f8bf4bd8a
nishantwhiz <nishant.jain12@outlook.com>
2023-07-19 22:06:04 +0530

    cors url changed to frontend vercel url

--- server/app.js
+++ server/app.js
@@ -22,7 +22,12 @@ const contactUsRoute = require("./routes/contact");
 // middleware
 app.use(express.json());
 app.use(cookieParser());
-app.use(cors({ origin: "http://localhost:3000", credentials: true }));
+app.use(
+  cors({
+    origin: "https://studynotion-megaproject.vercel.app",
+    credentials: true,
+  })
+);
 app.use(fileUpload({ useTempFiles: true, tempFileDir: "/tmp" }));
 //routes
 //default route
